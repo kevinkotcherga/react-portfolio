3237fcf2635c744fcbe69e9bbd5da034554ee928
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-09 20:54:54 +0100

    ✨feat(intro): création de la page d'intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import About from "./components/intro/about/About";
+import Intro from "./components/intro/Intro";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Intro />
+      <About />
+    </div>
+  )
 };
 
 export default App;
