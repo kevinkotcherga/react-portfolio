02b37133e1005db484e494ecc77bea5137d8b52f
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-17 10:45:53 +0100

    ✨feat(Media Queries): version mobile contact

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,9 +16,9 @@ const About = () => {
 
       <div className="a-left">
         <h1 data-aos='fade-up' className="a-title">A PROPOS DE MOI</h1>
-        <p  className="a-sub">Il y a quelques mois j'ai découvert les joies du code.</p>
-        <p  className="a-desc">Je me suis reconverti en Développeur Fullstack Ruby on Rails <br/>grâce au bootcamp de 9 semaines au Wagon.</p>
-        <p  className="a-desc-bottom">Aujourd'hui je parfaire mes compétences <br/> et me spéciale dans <span className='bold-span'>Node.js</span> et <span className='bold-span'>React</span>.</p>
+        <p data-aos='fade-up' className="a-sub">Il y a quelques mois j'ai découvert les joies du code.</p>
+        <p data-aos='fade-up' className="a-desc">Je me suis reconverti en Développeur Fullstack Ruby on Rails <br/>grâce au bootcamp de 9 semaines au Wagon.</p>
+        <p data-aos='fade-up' className="a-desc-bottom">Aujourd'hui je parfaire mes compétences <br/> et me spéciale dans <span className='bold-span'>Node.js</span> et <span className='bold-span'>React</span>.</p>
       </div>
 
        <div className="a-right">
