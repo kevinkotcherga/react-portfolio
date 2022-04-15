44b6da8545c16b6f1fe913248d79106e4df1a258
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-15 12:14:55 +0200

    feat(AOS) : modification des temps d'apparitions

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,10 +15,10 @@ const About = () => {
     <div className="a" id="profil">
 
       <div className="a-left">
-        <h1 className="a-title" data-aos='fade-up'>A PROPOS DE MOI</h1>
-        <p className="a-sub" data-aos='fade-up' data-aos-duration="1000">Il y a quelques mois j'ai découvert les joies du code.</p>
-        <p className="a-desc" data-aos='fade-up' data-aos-duration="2000">Je me suis reconverti en Développeur Fullstack Ruby on Rails <br/>grâce au bootcamp de 9 semaines au Wagon.</p>
-        <p className="a-desc-bottom" data-aos='fade-up' data-aos-duration="3000">Aujourd'hui je parfaire mes compétences et réalise <br/>le<span className='bold-span'> maximum</span> de projet pour <span className='bold-span'>m'améliorer</span>.</p>
+        <h1 className="a-title">A PROPOS DE MOI</h1>
+        <p className="a-sub" data-aos-duration="1000">Il y a quelques mois j'ai découvert les joies du code.</p>
+        <p className="a-desc" data-aos-duration="2000">Je me suis reconverti en Développeur Fullstack Ruby on Rails <br/>grâce au bootcamp de 9 semaines au Wagon.</p>
+        <p className="a-desc-bottom" data-aos-duration="3000">Aujourd'hui je parfaire mes compétences et réalise <br/>le<span className='bold-span'> maximum</span> de projet pour <span className='bold-span'>m'améliorer</span>.</p>
       </div>
 
        <div className="a-right">
