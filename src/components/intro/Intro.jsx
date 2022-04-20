77551124ee2d0e55a8134296ad446a23cf4b586e
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-20 16:31:38 +0200

    feat(intro) : modification du text d'intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -26,7 +26,7 @@ const Intro = () => {
               Kevin Kotcherga
             </div>
             <div data-aos='fade-up' data-aos-duration="1500" className="i-stack-header">
-              Frontend React.js, Next.js & Typescript
+              Frontend React.js & Typescript
             </div>
             <div data-aos='fade-up' data-aos-duration="2000" className="i-city-header">
               Basé à Paris
