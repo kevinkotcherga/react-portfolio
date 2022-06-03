2da538064fb1bf9937a797e9ca101d6fa23a8c34
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-03 14:58:45 +0200

    feat(about) : modification de la description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
 
       <div className="a-left">
         <h1 className="a-title" data-aos='fade-up' data-aos-duration="1000" data-aos-offset="-500" data-aos-anchorPlacement='top' >A PROPOS DE MOI</h1>
-        <p className="a-sub" data-aos='fade-up' data-aos-duration="1500" data-aos-offset="-700">Il y a quelques mois j'ai découvert les joies du code.</p>
-        <p className="a-desc" data-aos='fade-up' data-aos-duration="2000" data-aos-offset="-700">Je me suis reconverti en Développeur Web Fullstack grâce <br/> au bootcamp du Wagon.</p>
-        <p className="a-desc-bottom" data-aos='fade-up' data-aos-duration="2500" data-aos-offset="-700">Aujourd'hui je réalise le <span className='bold-span'> maximum </span>de projets pour gagner en <br/><span className='bold-span'> compétences</span> et continuer à <span className='bold-span'>progresser</span>.</p>
+        <p className="a-sub" data-aos='fade-up' data-aos-duration="1500" data-aos-offset="-700">Je développe depuis plus d'un 1 an différents projets et applications.</p>
+        <p className="a-desc" data-aos='fade-up' data-aos-duration="2000" data-aos-offset="-700">J'ai été diplomé de l'école le Wagon et obtenu mon bac +4 en informatique.</p>
+        <p className="a-desc-bottom" data-aos='fade-up' data-aos-duration="2500" data-aos-offset="-700">Aujourd'hui je réalise le <span className='bold-span'> maximum </span>de projets pour gagner en <span className='bold-span'> compétences</span> et continuer à <span className='bold-span'>progresser</span>.</p>
       </div>
 
        <div className="a-right">
