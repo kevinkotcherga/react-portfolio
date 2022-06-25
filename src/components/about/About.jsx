c014e4a3d80ba3e13ab925078b72fbc9c4521b31
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-25 18:04:03 +0200

    feat(component) : modification du a propos

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h1 className="a-title" data-aos='fade-up' data-aos-duration="1000" data-aos-offset="-500" data-aos-anchorPlacement='top' >A PROPOS DE MOI</h1>
         <p className="a-sub" data-aos='fade-up' data-aos-duration="1500" data-aos-offset="-700">Développeur depuis plus d'un 1 an j'ai réalisé de nombreuses applications et différents projets</p>
         <p className="a-desc" data-aos='fade-up' data-aos-duration="2000" data-aos-offset="-700">Diplomé de l'école le Wagon, j'ai obtenu mon bac +4 et je prépare un nouveau diplôme à la Wild Code School </p>
-        <p className="a-desc-bottom" data-aos='fade-up' data-aos-duration="2500" data-aos-offset="-700">Aujourd'hui je continue à créer un <span className='bold-span'> maximum </span>de projets pour gagner en <span className='bold-span'> compétences</span> et continuer à <span className='bold-span'>progresser</span></p>
+        <p className="a-desc-bottom" data-aos='fade-up' data-aos-duration="2500" data-aos-offset="-700">Aujourd'hui je reste informé des dernières<span className='bold-span'> nouveautés </span> et je construis des interfaces <span className='bold-span'> modernes </span> et <span className='bold-span'>ergonomiques</span></p>
       </div>
 
        <div className="a-right">
