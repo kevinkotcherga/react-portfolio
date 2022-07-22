d8effc1516338ad5bfca4e2af327c2efacaac978
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-07-22 15:56:09 +0200

    feat(components) : Ajout des links vers les projets depuis projectList

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import { useLocation } from 'react-router-dom';
+import CustomCursor from '../components/CustomCursor';
 
 const Project = () => {
+	const location = useLocation();
+	const id = location.pathname.split('/')[2];
+	console.log(id);
 	return (
 		<div>
+			<CustomCursor />
 			<h1>Project</h1>
 		</div>
 	);
